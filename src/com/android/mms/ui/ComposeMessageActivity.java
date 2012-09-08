e73d771e5212a8e2cc0582c86ada94aa94f3a9b0
DvTonder <david.vantonder@gmail.com>
2012-09-07 21:02:26 -0400

    QuickMessage: Cleanup, optimization and bug fixes
    This commit fixes the multiple ghost messages bug.  It also includes
    some general cleanup and optimization.
    
    Change-Id: I049bcb7df30e79279a28e04f90f9a96a7de94004

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -2149,7 +2149,7 @@ public class ComposeMessageActivity extends Activity
         // the thread. Unblocking occurs when we're done querying for the conversation
         // items.
         mConversation.blockMarkAsRead(true);
-        mConversation.markAsRead();         // dismiss any notifications for this convo
+        mConversation.markAsRead(true);         // dismiss any notifications for this convo
         startMsgListQuery();
         updateSendFailedNotification();
         drawBottomPanel();
@@ -3817,7 +3817,7 @@ public class ComposeMessageActivity extends Activity
 
     private void checkPendingNotification() {
         if (mPossiblePendingNotification && hasWindowFocus()) {
-            mConversation.markAsRead();
+            mConversation.markAsRead(true);
             mPossiblePendingNotification = false;
         }
     }
